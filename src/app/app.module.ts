cc08807cb2c47a66a3bcbd2c7f9a913cf74518bf
sergiovl219 <sergiovl219@gmail.com>
2020-10-04 00:13:59 -0500

    Se agregaron métodos para consumir los servicios de back.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +23,13 @@ import { SharedService } from './shared.service';
     ShowEplComponent,
     AddEditEplComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [SharedService],
   bootstrap: [AppComponent],
 })
--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
+// TODO: Puede quedar en settigs? Para poderse importar si es STG, QA, PROD.
+readonly APIUrl = 'http://127.0.0.1:8000/';
+readonly PhotoUrl = 'http://127.0.0.1:8000/media';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getEmpresas():Observable<any[]>{
+    return this.http.get<any[]>(this.APIUrl + '/empresa/');
+  }
+
+  getNombresEmpresas():Observable<any[]>{
+    return this.http.get<any[]>(this.APIUrl + '/empresa/');
+  }
+
+  addEmpresa(val:any){
+    return this.http.post(this.APIUrl + '/empresa/', val);
+  }
+
+  updateEmpresa(val:any){
+    return this.http.put(this.APIUrl + '/empresa/', val);
+  }
+
+  deleteEmpresa(val:any){
+    return this.http.delete(this.APIUrl + '/empresa/' + val);
+  }
+
+  getEmpleados():Observable<any[]>{
+    return this.http.get<any[]>(this.APIUrl + '/empleado/');
+  }
+
+  addEmpleado(val:any){
+    return this.http.post(this.APIUrl + '/empleado/', val);
+  }
+
+  addEmpleadoFoto(val:any){
+    return this.http.post(this.APIUrl + '/empleado/foto', val);
+  }
+
+  updateEmpleado(val:any){
+    return this.http.put(this.APIUrl + '/empleado/', val);
+  }
+
+  deleteEmpleado(val:any){
+    return this.http.delete(this.APIUrl + '/empleado/' + val);
+  }
 }
