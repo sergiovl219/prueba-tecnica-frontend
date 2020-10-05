90db03e1e510eb349f9ce140eb2682b3a13ffc79
sergiovl219 <sergiovl219@gmail.com>
2020-10-04 19:51:42 -0500

    Routing de los modulos empleado y empresa.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { EmpleadoComponent } from './empleado/empleado.component'; 
+import { EmpresaComponent } from './empresa/empresa.component';
+
+const routes: Routes = [
+  {path:'empleado', component:EmpleadoComponent},
+  {path:'empresa', component:EmpresaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
